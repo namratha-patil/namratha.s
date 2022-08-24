04a60efeddf6957fa5bd25585f45fa96b1c2d6fe
namratha-patil <88672574+namratha-patil@users.noreply.github.com>
2022-08-24 03:37:57 -0700

    Add files via upload

---
+++ asst2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class asst2
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("Input a first number: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input a second number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int minNumber;
+            if (m<n)
+                {
+                minNumber = m;
+            }
+            else
+            {
+                minNumber = n;
+            }
+            Console.WriteLine("minimum nuber is:" + minNumber);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ asst6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class asst6
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Current Date and Time is : ");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+        }
+
+    }
+}
