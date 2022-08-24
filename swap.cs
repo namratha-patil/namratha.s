57f83faf399f7b72e549e4856677d8de72352d8e
namratha-patil <88672574+namratha-patil@users.noreply.github.com>
2022-08-24 03:37:11 -0700

    Add files via upload

---
+++ swap.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace trainee
+{
+    internal class swap
+    {
+        static void SwapNum(ref int x, ref int y)
+        {
+
+            int tempswap = x;
+            x = y;
+            y = tempswap;
+        }
+        public static void Main()
+        {
+
+            int x = 10, y = 20;
+            Console.WriteLine($"Before swapping{x},{y}");
+            SwapNum(ref x, ref y);
+            Console.WriteLine($"After swapping{x},{y}");
+
+        }
+    }
+}
